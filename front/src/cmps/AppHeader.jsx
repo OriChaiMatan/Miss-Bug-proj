c9c2731ff8c2499edaa317d37808aef0c39313ec
OriChaiMatan <ori.chaimatan@gmail.com>
2024-05-05 20:42:43 +0300

    Add users nav-link in appheader

--- front/src/cmps/AppHeader.jsx
+++ front/src/cmps/AppHeader.jsx
@@ -9,7 +9,9 @@ export function AppHeader() {
             
             <div className='header-container'>
                 <nav className='app-nav'>
-                    <NavLink to="/">Home</NavLink> |<NavLink to="/bug">Bugs</NavLink> |
+                    <NavLink to="/">Home</NavLink> |
+                    <NavLink to="/bug">Bugs</NavLink> |
+                    <NavLink to="/user">Users</NavLink>|
                     <NavLink to="/about">About</NavLink>
                 </nav>
                 <h1>Bugs are Forever</h1>
