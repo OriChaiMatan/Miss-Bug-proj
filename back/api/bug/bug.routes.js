daf0d42ddfdae99693dfdb847e0c41ce0dafc520
OriChaiMatan <ori.chaimatan@gmail.com>
2024-05-14 13:54:22 +0300

    Add simple Log middleware

--- back/api/bug/bug.routes.js
+++ back/api/bug/bug.routes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { addBug, getBugs, getBug, removeBug, updateBug } from './bug.controller.js'
+import { log } from '../../middlewares/log.middleware.js'
 
 const router = express.Router()
 
-router.get('/', getBugs)
-router.get('/:bugId', getBug)
-router.delete('/:bugId', removeBug)
-router.put('/:bugId', updateBug)
-router.post('/', addBug)
+router.get('/', log, getBugs)
+router.get('/:bugId', log, getBug)
+router.delete('/:bugId', log, removeBug)
+router.put('/:bugId', log, updateBug)
+router.post('/', log, addBug)
 
 export const bugRoutes = router
